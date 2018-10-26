40afd2109e1dc9edfa05011b85130355261e6e83
Jacklama <37448755+Jacklama@users.noreply.github.com>
2018-10-26 10:32:02 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
-#inlcude <iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
-  int myArray[24] = {2,32,42434,243,243,44,2,324,34,24,343,43,4,3,34,3,45,25,55,25,77,65,55,445,44};
+  int myArray[24] = {2,32,42434,243,243,44,2,324,34,24,343,43,4,3,34,3,45,25,55,25,77,65,55,445};
   cout << sizeof(myArray)/sizeof(myArray[0]);
-return 0;
+  return 0;
 }
