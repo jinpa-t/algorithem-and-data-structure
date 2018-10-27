f5c3ede691f9b56f2061c16b698422f63929f03c
Jacklama <37448755+Jacklama@users.noreply.github.com>
2018-10-27 18:58:41 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 using namespace std;
 
+void sort_array(int passed_Array[], int passed_size){
+  int temp, i , j ;
+  for (i = 0; i < passed_size; i++){
+    for (j = 0; j < passed_size; j++) {
+      if(passed_Array[i] < passed_Array[j])
+      {
+        temp = passed_Array[i];
+        passed_Array[i] = passed_Array[j];
+        passed_Array[j] = temp;
+      }
+      
+    }
+    
+  }
+  for(i = 0; i < passed_size; i++)
+  {
+      cout << passed_Array[i] << " ";
+  }
+  
+}
 int main() {
-  int myArray[24] = {2,32,42434,243,243,44,2,324,34,24,343,43,4,3,34,3,45,25,55,25,77,65,55,445};
-  cout << sizeof(myArray)/sizeof(myArray[0]);
+  int myArray[24] = {2,32,434,243,243,44,2,324,34,24,343,43,4,3,34,3,45,25,55,25,77,65,55,445};
+  int array_size =  sizeof(myArray)/sizeof(myArray[0]);
+  sort_array(myArray, array_size); //passing array to function 
   return 0;
 }
