5aa8fe05f8607168c9c36dcae6a8b84ac25e3053
Jacklama <37448755+Jacklama@users.noreply.github.com>
2019-06-06 09:30:29 -0400

    Create Stack list.cpp

---
+++ Stack list.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+struct Node{
+	int data;
+	Node *link;
+};
+Node *top = NULL;
+bool isempty(){
+	if(top == NULL)
+	return true;
+	else
+	return false;
+}
+
+void push(int value){
+	Node *ptr = new Node();
+	ptr->data= value;
+	ptr->link= top;
+	top = ptr;
+}
+
+void pop(){
+	if(isempty())
+		cout <<"Stack is empty";
+	else {
+		Node *ptr = top;
+		top = top->link;
+		delete(ptr);
+	}
+}
+
+void showTop(){
+	if (isempty())
+		cout << "Stack is empty";
+		else 
+		cout <<"Element at top is " << top->data;
+	
+}
+int main(){
+	push(1);
+	push(2);
+	pop();
+  showTop();
+	return 0;
+}
