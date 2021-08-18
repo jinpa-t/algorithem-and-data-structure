4a155b1c4773494b4185eadecc86a5c7939b675c
J Tsering <37448755+tseringj@users.noreply.github.com>
2021-08-18 13:52:09 -0400

    format change

--- Recursive Binary Search.cpp
+++ Recursive Binary Search.cpp
@@ -6,23 +6,22 @@ int binarySearch(vector<int>&myvector, int l, int r, int x) // l = left, r = rig
 { 
     if (r >= l) 
     { 
-		int mid = l + (r - l)/2; 
-		if (myvector[mid] == x) 
-			return mid; 
-		if (myvector[mid] > x) 
-			return binarySearch(myvector, l, mid-1, x); 
-		return binarySearch(myvector, mid+1, r, x); 
+	int mid = l + (r - l)/2; 
+	if (myvector[mid] == x) 
+		return mid; 
+	if (myvector[mid] > x) 
+		return binarySearch(myvector, l, mid-1, x); 
+	return binarySearch(myvector, mid+1, r, x); 
     } 
     return -1; 
 } 
 
 int main ()
 {
-    vector<int> myvector ={2,5,7,11,20,30,40,55,60};
+	vector<int> myvector ={2,5,7,11,20,30,40,55,60};
 	int end = myvector.size();
-	
 	int find =40;
-    cout << "Found at index: " << binarySearch(myvector,0,end-1,find);
+	cout << "Found at index: " << binarySearch(myvector,0,end-1,find);
 
-    return 0;
+	return 0;
 }
