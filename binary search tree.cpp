c89b92bb1b498b2b8141535f296b989f57148b38
J Tsering <37448755+tseringj@users.noreply.github.com>
2021-08-18 13:25:00 -0400

    minor updates

--- binary search tree.cpp
+++ binary search tree.cpp
@@ -24,7 +24,6 @@ node * insert(node* root, int key){
     else
     ;
     return root;
-    
 }
 
 bool Bfind(node* root, int key){
@@ -41,37 +40,10 @@ bool Bfind(node* root, int key){
         return false;
 }
 
-void displayVector(const vector<int> & vec){
-    
-    for(int i=0 ; i< vec.size(); i++){
-        cout <<  vec[i] << " ";
-    }
-    cout << endl;
-}
-
-void insertionSort(vector<int> & vec){
-
-    int i, temp, j; 
-   for (i = 1; i < vec.size(); i++)
-   { 
-       temp = vec[i];
-       for (j = i; j > 0 && temp < vec[j-1]; j--) 
-       { 
-           vec[j] = vec[j-1];
-       }
-       vec[j] = temp;  
-   }
-}
 
 int main(int argc, const char * argv[]) {
     vector<int> myVec {77,200,99,57,69,45,101,67,120,132,87,157,170,12};
-    cout << "Unsorted array is  :"<< endl;
-    displayVector(myVec);
-    
-    cout << "Sorted array is  :"<< endl;
-    insertionSort(myVec);
-    displayVector(myVec);
-    
+       
     //insert to tree
     node * root = new node(myVec[0]);
     for (int i = 1; i < myVec.size(); i++) {
@@ -81,13 +53,10 @@ int main(int argc, const char * argv[]) {
     int userInput;
     cout << "Enter the number that you would like to find : " << endl;
     cin>> userInput;
-    Bfind(root, userInput);
-    
-    if (Bfind(root, userInput) == 0) {
-        cout << "The number " << userInput << " cannot be found!" << endl;
-    }
-    else 
-        cout << "The number " << userInput << " found at index " << Bfind(root, userInput) << endl;
+    if (Bfind(root, userInput))
+        cout << "The number " << userInput << " found" << endl;
+    else
+        cout << "The number " << userInput << " not found" << endl;
     
     return 0;
         
