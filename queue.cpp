4d42a6789d96181ab0a082910badbc328ac0faa3
J Tsering <37448755+tseringj@users.noreply.github.com>
2021-09-08 10:22:18 -0400

    new version
    Implementation of queue using struct and class

--- queue.cpp
+++ queue.cpp
@@ -1,28 +1,76 @@
 #include <iostream>
-#include <queue>
 using namespace std;
+struct Node{
+	int data;
+	Node *link = nullptr;
+};
+class Queue{
+    private:
+	int size = 0;
+    Node *front = NULL;
+    Node *back = NULL;
+    
+    public:
+    bool isempty(){
+    	if(front == NULL)
+    	return true;
+    	else
+    	return false;
+    }
+    int get_front(){
+        if(isempty())
+            return front->data;
+        return -999;
+    }
+    //insert at the back of the link
+    void enqueue(int value){ 
+    	if(size == 0){
+    	    Node *ptr = new Node();
+        	ptr->data= value;
+        	ptr->link= front;
+        	front = ptr;
+        	back = ptr;
+    	} else {
+    	    Node *ptr = new Node();
+    	    ptr->data= value;
+    	    if (back != NULL){
+    	        back->link = ptr;
+    	        ptr->link= NULL;
+    	        back = ptr;
+    	    }
+    	}
+    	size++;
+    }
+    //remove at the front of the link
+    void dequeue(){
+    	if(isempty())
+    		cout <<"Queue is empty\n";
+    	else {
+    		Node *ptr = front;
+    		cout << "Dequeued: "<< ptr->data << "\n";
+    		front = front->link;
+    		delete(ptr);
+    		size--;
+    	}
+    }
+    void showFront(){
+    	if (isempty())
+    		cout << "Queue is empty";
+    	else
+    		cout <<"Element at the Front: " << front->data << "\n";
+    }
+};
 
-int main()
-{
-  queue<int> myqueue;
-  	
- 	int vals[] = {5,6,7,10,3,2,0,9, 15, 23}; // vlaues
- 	int size = sizeof(vals) / sizeof(vals[0]);
-  for(int i = 0; i < size ;i++ ) {
-     myqueue.push(vals[i]);
-  }
-  	
-	cout << "Size of myQueue is : " << myqueue.size();
-	cout << endl;
-	cout << "Front Element: " << myqueue.front();
-	cout << endl;
-	cout << "Back Element: " << myqueue.back();
-
-	cout << endl;
-	while (!myqueue.empty())
-  	{
-	    cout<< myqueue.front() << " ";
-	    myqueue.pop();
-  	}
+int main(){
+    int vals[] = {5,6,7,10,3,2,0,9,15,23};
+    int size = sizeof(vals) / sizeof(vals[0]);
+    Queue myQueue;
+    for(int i = 0; i < size ;i++ ) {
+        myQueue.enqueue(vals[i]);
+    }
+    // remove two element
+	myQueue.dequeue();
+	myQueue.dequeue();
+  	myQueue.showFront();
 	return 0;
 }
